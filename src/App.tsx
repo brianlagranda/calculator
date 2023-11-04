b21dabab1e56483e80e46a6b4bb9e3f668369edd
bra20019613 <brianlagranda@gmail.com>
2023-11-03 23:15:19 -0300

    Feat: Keypad onClick

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Calculator from './components/Calculator';
 function App() {
   return (
     <main className='mx-auto mt-32 max-w-xl rounded-lg bg-slate-600 p-6'>
-      <div className='flex w-full items-center justify-between px-4'>
+      <div className='flex w-full items-center justify-between px-4 cursor-default'>
         <h1 className='text-3xl font-bold text-white'>calc</h1>
         <ThemeSelector />
       </div>
--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,27 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Display from './Display';
 import Keypad from './Keypad';
 
 export default function Calculator() {
-  const [input, setInput] = useState<string>('');
+  const [prevInput, setPrevInput] = useState<string>('');
   const [currentInput, setCurrentInput] = useState<string>('');
-  const [PrevInput, setPrevInput] = useState<string>('');
   const [operator, setOperator] = useState<string | null>(null);
   const [calculatedResult, setCalculatedResult] = useState<string>('0');
+  const [record, setRecord] = useState<string[]>([]);
 
-  const add = (a: string, b: string): number => Number(a + b);
+  const operators: string[] = ['+', '-', 'x', '/'];
 
-  const subtract = (a: string, b: string): number => Number(a - b);
+  const add = (a: string, b: string): number => Number(a) + Number(b);
 
-  const multiply = (a: string, b: string): number => Number(a * b);
+  const subtract = (a: string, b: string): number => Number(a) - Number(b);
 
-  const divide = (a: string, b: string): number => Number(a / b);
+  const multiply = (a: string, b: string): number => Number(a) * Number(b);
 
-  const deleteLastChar = (calculatedResult: string) => {
-    return setCalculatedResult(calculatedResult.slice(0, -1));
+  const divide = (a: string, b: string): number => Number(a) / Number(b);
+
+  const equals = (
+    calculatedResult: string,
+    currentInput: string,
+    record: string[]
+  ) => {
+    console.log('Calcu: ', calculatedResult, 'curr: ', currentInput);
+    switch (operator) {
+      case '+':
+        return setCalculatedResult(String(add(calculatedResult, currentInput)));
+      case '-':
+        return setCalculatedResult(
+          String(subtract(calculatedResult, currentInput))
+        );
+      case '+':
+        return setCalculatedResult(
+          String(multiply(calculatedResult, currentInput))
+        );
+      case '-':
+        return setCalculatedResult(
+          String(divide(calculatedResult, currentInput))
+        );
+    }
+    console.log('calculatedResult Inside:', calculatedResult);
+  };
+
+  const deleteLastChar = (input: string) => {
+    return setInput(input.slice(0, -1));
   };
 
   const clearAll = () => {
+    setInput('');
     setPrevInput('');
     setCurrentInput('0');
     setOperator(null);
@@ -30,35 +58,53 @@ export default function Calculator() {
 
   const handleClick = (value: string) => {
     if (value >= '0' && value <= '9') {
-      setInput(input + value);
+      setCurrentInput((currentInput) => currentInput + value);
     } else {
-      setOperator(value);
-      setPrevInput(input);
+      const lastInput = record[record.length - 1];
+      if (operators.includes(value) && currentInput !== '') {
+        setPrevInput(currentInput);
+        setRecord((record) => [...record, currentInput, value]);
+        setOperator(value);
+        setCurrentInput('');
+      } else if (lastInput !== currentInput) {
+        setRecord((record) => [...record, currentInput]);
+      }
+    }
+
+    switch (value) {
+      case 'DEL':
+        return deleteLastChar(currentInput);
+      case 'RESET':
+        return clearAll();
+      case '+':
+        return setCalculatedResult(String(add(calculatedResult, currentInput)));
+      case '-':
+        return setCalculatedResult(
+          String(subtract(calculatedResult, currentInput))
+        );
+      case '+':
+        return setCalculatedResult(
+          String(multiply(calculatedResult, currentInput))
+        );
+      case '-':
+        return setCalculatedResult(
+          String(divide(calculatedResult, currentInput))
+        );
+      case '=':
+        return equals(calculatedResult, currentInput, record);
     }
   };
 
+  console.log(record);
+  console.log(prevInput);
+  console.log(currentInput);
+  console.log('calculatedResult Outside:', calculatedResult);
+
   return (
     <>
-      <Display result={input} />
+      <Display result={currentInput} />
       <Keypad onButtonClick={handleClick} />
     </>
   );
 }
-
-/*     switch (operator) {
-      case 'DEL':
-        return deleteLastChar(calculatedResult);
-      case 'RESET':
-        return clearAll();
-      case '+':
-        return add(PrevInput, currentInput);
-      case '-':
-        return subtract(PrevInput, currentInput);
-      case '/':
-        return divide(PrevInput, currentInput);
-      case 'x':
-        return multiply(PrevInput, currentInput);
-      case '=':
-        return alert('hi');
-    } */
 
