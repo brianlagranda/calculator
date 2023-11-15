5836cfdb6cba35d8e0ab637ee43ed663e1cc9198
bra20019613 <brianlagranda@gmail.com>
2023-11-15 20:33:37 -0300

    fixed: Wrong implementation of calcs.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -16,10 +16,10 @@ export default function Calculator() {
   const OPERATORS: string[] = ['+', '-', 'x', '/'];
   const NUMBERS: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-  const locale = 'en-US';
-  const numberFormatOptions = { style: 'decimal' };
+  // const locale = 'en-US';
+  // const numberFormatOptions = { style: 'decimal' };
 
-  const calculate = (prevResult: string, operator: string) => {
+  const calculate = (operator: string) => {
     let calculatedResult: number;
 
     console.log('Result: ', result);
@@ -27,39 +27,28 @@ export default function Calculator() {
 
     switch (operator) {
       case '+':
-        calculatedResult = add(prevResult, currentInput);
+        calculatedResult = add(result, currentInput);
         break;
       case '-':
-        calculatedResult = subtract(prevResult, currentInput);
+        calculatedResult = subtract(result, currentInput);
         break;
       case 'x':
-        calculatedResult = multiply(prevResult, currentInput);
+        calculatedResult = multiply(result, currentInput);
         break;
       case '/':
-        calculatedResult = divide(prevResult, currentInput);
+        const divisor = currentInput;
+        if (divisor !== '0') {
+          calculatedResult = divide(result, currentInput);
+        } else {
+          alert(`You can't divide by zero`);
+          calculatedResult = Number(result);
+        }
         break;
       default:
         calculatedResult = 0;
     }
 
-    return calculatedResult.toLocaleString(locale, numberFormatOptions);
-  };
-
-  const deleteLastChar = (input: string) => {
-    if (input.charAt(input.length - 2) === ',') {
-      setCurrentInput(input.slice(0, -2));
-    } else {
-      input.length === 1
-        ? setCurrentInput('0')
-        : setCurrentInput(input.slice(0, -1));
-    }
-  };
-
-  const clearAll = () => {
-    setCurrentInput('0');
-    setOperator('');
-    setResult('0');
-    setHistorial('');
+    return String(calculatedResult);
   };
 
   const handleClick = (value: string) => {
@@ -80,50 +69,40 @@ export default function Calculator() {
     if (currentInput === '0') {
       setCurrentInput(value);
     } else if (currentInput.length < 21) {
-      const numericValue = parseFloat(currentInput.replace(/,/g, ''));
+      //const numericValue = parseFloat(currentInput.replace(/,/g, ''));
 
       let newValue;
+
       if (value === '.' && !currentInput.includes('.')) {
-        newValue = `${numericValue}${value}`;
+        newValue = `${currentInput}${value}`;
       } else if (value !== '.') {
-        newValue = `${numericValue}${value}`;
+        newValue = `${currentInput}${value}`;
       } else {
-        newValue = `${numericValue}`;
+        newValue = currentInput;
       }
 
-      const formattedValue = parseFloat(newValue).toLocaleString(
-        locale,
-        numberFormatOptions
-      );
-      setCurrentInput(formattedValue);
+      setCurrentInput(newValue);
     }
   };
 
   const handleOperatorInput = (value: string) => {
-    console.log('heree: ', operator);
-    console.log('heree: ', value);
-    console.log('heree result: ', result);
-    if (currentInput !== '0') {
-      setResult((prevResult) => calculate(prevResult, value));
-      setHistorial(`${result === '0' ? currentInput : result} ${value}`);
-      setCurrentInput('0');
-    } else {
+    if (operator === '') {
+      setResult(currentInput);
       setHistorial(`${currentInput} ${value}`);
-      setCurrentInput('0');
+    } else if (currentInput === '0') {
+      setHistorial(`${result} ${value}`);
+    } else {
+      setResult(calculate(operator));
+      setHistorial(`${result} ${value}`);
     }
     setOperator(value);
+    setCurrentInput('0');
   };
 
   const handleOtherInput = (value: string) => {
     if (value !== 'DEL' && value !== '=') {
+      console.log('other output');
     }
-  };
-
-  const handleEquals = (operator: string) => {
-    console.log('operatoooor: ', operator);
-
-    setResult((prevResult) => calculate(prevResult, operator));
-    setCurrentInput('0');
   };
 
   const handleSpecialInputs = (value: string) => {
@@ -135,11 +114,35 @@ export default function Calculator() {
         clearAll();
         break;
       case '=':
-        handleEquals(operator);
+        handleEquals(value);
         break;
       default:
         break;
     }
+  };
+
+  const handleEquals = (value: string) => {
+    operator === ''
+      ? setHistorial(`${result} ${value}`)
+      : setHistorial(`${result} ${operator} ${currentInput} ${value}`);
+    setResult(calculate(operator));
+  };
+
+  const deleteLastChar = (input: string) => {
+    if (input.charAt(input.length - 2) === ',') {
+      setCurrentInput(input.slice(0, -2));
+    } else {
+      input.length === 1
+        ? setCurrentInput('0')
+        : setCurrentInput(input.slice(0, -1));
+    }
+  };
+
+  const clearAll = () => {
+    setCurrentInput('0');
+    setOperator('');
+    setResult('0');
+    setHistorial('');
   };
 
   return (
