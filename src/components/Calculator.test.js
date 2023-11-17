0f7d472fa0e3b4d753063c0d71879b3ff0b85c00
bra20019613 <brianlagranda@gmail.com>
2023-11-17 11:12:37 -0300

    fix: Convert test inputs to strings.

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -9,19 +9,19 @@ describe('Renders main page correctly', async () => {
 
 describe('Calculating results', () => {
   it('should add 5 and 3 to equal 8', () => {
-    expect(add(5, 3)).toBe(8);
+    expect(add('5', '3')).toBe(8);
   });
 
   it('should add 5 and 3 to equal 8', () => {
-    expect(subtract(10, 4)).toBe(6);
+    expect(subtract('10', '4')).toBe(6);
   });
 
   it('should add 5 and 3 to equal 8', () => {
-    expect(multiply(8, 4)).toBe(32);
+    expect(multiply('8', '4')).toBe(32);
   });
 
   it('should add 5 and 3 to equal 8', () => {
-    expect(divide(100, 4)).toBe(25);
+    expect(divide('100', '4')).toBe(25);
   });
 });
 
